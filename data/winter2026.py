8baeca403fb5e44aa163efa9d8ebe51a04d0ef81
Jackson Nace <jacksonnace964@gmail.com>
2025-12-30 08:15:34 -0600

    add 2 games

--- data/winter2026.py
+++ data/winter2026.py
@@ -42,4 +42,29 @@ games_v3 = [
     'team2': 'White',
     'team2_players': ['Jackson', 'Roberto', 'Joe', 'Landon', 'Quan'],
     'winning_team': 'Black' },
+  #December 23rd, 2025, Black 27 - 35 White
+  { 'team1': 'White',
+    'team1_players': ['Joe', 'Quan', 'Landon', 'JJ', 'Roberto'],
+    'team2': 'Black',
+    'team2_players': ['Jared', 'Waltman', 'Chance', 'Jackson', 'ChrisNorman'],
+    'winning_team': 'White' },
+    #December 29th, 2025, Black 48 - 45 White
+  {   'team1': 'Black',
+      'team1_players': ['Hernan', 'Mason', 'Sam', 'Neftaly', 'ChrisNorman'],
+      'team2': 'White',
+      'team2_players': ['Joe', 'Jared', 'Waltman', 'ChrisLeger', 'Paolo'],
+      'winning_team': 'Black',
+          'player_stats': {
+          'Hernan': {'points': 12, '2pt_m': 6, '2pt_a': 10, '3pt_m': 0, '3pt_a': 5},
+          'Mason': {'points': 10, '2pt_m': 5, '2pt_a': 14, '3pt_m': 0, '3pt_a': 4},
+          'Sam': {'points': 12, '2pt_m': 6, '2pt_a': 8, '3pt_m': 0, '3pt_a': 1},
+          'Neftaly': {'points': 0, '2pt_m': 0, '2pt_a': 1, '3pt_m': 0, '3pt_a': 1},
+          'ChrisNorman': {'points': 14, '2pt_m': 4, '2pt_a': 10, '3pt_m': 2, '3pt_a': 6},
+          'Joe': {'points': 10, '2pt_m': 5, '2pt_a': 11, '3pt_m': 0, '3pt_a': 3},
+          'Jared': {'points': 18, '2pt_m': 3, '2pt_a': 8, '3pt_m': 4, '3pt_a': 9},
+          'Waltman': {'points': 2, '2pt_m': 1, '2pt_a': 3, '3pt_m': 0, '3pt_a': 3},
+          'ChrisLeger': {'points': 10, '2pt_m': 5, '2pt_a': 10, '3pt_m': 0, '3pt_a': 5},
+          'Paolo': {'points': 5, '2pt_m': 1, '2pt_a': 2, '3pt_m': 1, '3pt_a': 1}
+      }
+  },
 ]
